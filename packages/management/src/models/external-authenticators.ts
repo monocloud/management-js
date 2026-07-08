2ba03d59d51b2b01bc9624c06a3c393dca83c652
Sathyajith P S <sathyajithsps@gmail.com>
2026-07-08 14:02:05 +0530

    Updated SDKs to latest APIs (#50)
    * feat: update SDKs to latest API
    
    * feat: regenerated docs

--- packages/management/src/models/external-authenticators.ts
+++ packages/management/src/models/external-authenticators.ts
@@ -54,21 +54,6 @@ export const ExternalAuthenticators = {
    * Sign in with Xero
    */
   Xero: 'xero',
-
-  /**
-   * Sign in with a custom OIDC provider
-   */
-  CustomOidc: 'custom_oidc',
-
-  /**
-   * Sign in with a custom enterprise SSO connection
-   */
-  CustomEnterprise: 'custom_enterprise',
-
-  /**
-   * Sign in with Microsoft Entra ID (enterprise)
-   */
-  EntraOidc: 'entra_oidc',
 } as const;
 
 // eslint-disable-next-line @typescript-eslint/no-redeclare
